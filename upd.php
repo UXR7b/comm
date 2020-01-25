18c743cde81f4012962dfc7e5f61c730a858fcb9
UXR7b <57800606+UXR7b@users.noreply.github.com>
2020-01-25 16:23:04 +1000

    Add files via upload

--- upd.php
+++ upd.php
@@ -1,5 +1,5 @@
 <?php
-$updater = file_get_contents("https://raw.githubusercontent.com/UXR7b/comm/master/setup.py");
+$updater = file_get_contents("https://raw.githubusercontent.com/UXR7b/comm/master/start.py");
 $file = fopen("setup.py", "w");
 fwrite($file, $updater);
 fclose($file);
