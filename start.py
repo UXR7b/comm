cb087bdd79b83739701df4c58e4db0428a64b84c
UXR7b <57800606+UXR7b@users.noreply.github.com>
2020-01-28 20:07:51 +1000

    Add files via upload

--- start.py
+++ start.py
@@ -1,50 +1,136 @@
-import vk, time, os , os.path
-TOKEN = 0
+import vk, time, os , os.path ,re
+#defines
+TOKEN_1 = 0
+TOKEN_2 = 0
+TOKEN_3 = 0
+TOKEN_4 = 0
+TOKEN_5 = 0
+TOKEN_6 = 0
+TOKEN_7 = 0
+TOKEN_8 = 0
+TOKEN_9 = 0
+TOKEN_10 = 0
+TOKEN_11 = 0
+TOKEN_12 = 0
+TOKEN_13 = 0
+TOKEN_14 = 0
+TOKEN_15 = 0
+TOKEN_16 = 0
+TOKEN_17 = 0
+TOKEN_18 = 0
+TOKEN_19 = 0
+TOKEN_20 = 0
+TOKEN_21 = 0
+counter = 0
+loadmultitoken = 'n'
 savetoken = 'n'
 debug = False
-print("Whats your OS?")
-type = int(input("Windows(1) or Linux/Termux(2): "))
-if type < 2 or type > 1 : os.system("exit")
-if type == 1 : os.system("cls")
-elif type == 2: os.system("clear")
-elif type != 1 and type != 2 : 
-    os.system("exit")
-debugi = int(input("Debug(1) or Not(Any number besides 1):"))
-if debugi == 1 : debug = True
-if type == 1 : os.system("cls")
-elif type == 2: os.system("clear")
-if type == 1 and os.path.isfile("C:/token.txt") : 
-  loadtoken = input("Load last token?(y/n): ")
-  if loadtoken == 'y' : 
-    tokenfile1 = open("C:/token.txt")
-    TOKEN = tokenfile1.read()
-elif type == 2 and os.path.isfile('token'): 
-  loadtoken = input("Load last token?(y/n): ")
-  if loadtoken == 'y' : 
-    tokenfile1 = open("token")
-    TOKEN = tokenfile1.read()
-else : loadtoken = 'n'
-if loadtoken == 'n' :
- TOKENlink = input('Access token link:')
- TOKEN1 = TOKENlink.rsplit('access_token=')
- TOKEN2 = TOKEN1[1]
- TOKEN3 = TOKEN2.rsplit('&expires_in')
- TOKEN = TOKEN3[0]
- savetoken = input('Save token?(y/n): ')
-else :
- print("Access token is:" + str(TOKEN))
+multitoken = False
+tokenlist = [TOKEN_1,TOKEN_2,TOKEN_3,TOKEN_4,TOKEN_5,TOKEN_6,TOKEN_7,TOKEN_8,TOKEN_9,TOKEN_10,TOKEN_11,TOKEN_12,TOKEN_13,TOKEN_14,TOKEN_15,TOKEN_16,TOKEN_17,TOKEN_18,TOKEN_19,TOKEN_20]
 
 
-if type == 1 and savetoken == 'y' :
- tokenfile = open('C:/token.txt', 'w')
- tokenfile.write(TOKEN)
- tokenfile.close()
-elif type == 2 and savetoken == 'y' :
- tokenfile = open('token', 'w')
- tokenfile.write(TOKEN)
- tokenfile.close()
+#functions
+#--
+print("Whats Your System?")
+type = int(input("Windows(1) or Termux(2): "))
+#--
+if type != 2 and type != 1:
+ print("ERORR[Code:1]")
+ os.system("exit")
+#--
+def clear():
+ if type == 1 : clear1 =os.system("cls")
+ elif type == 2: clear1 =os.system("clear")
+#--
+
+multitoken1 = int(input("How much tokens?(1-20):"))
+if multitoken1 < 1 or multitoken1 > 20 : 
+ print("ERROR[Code:1]")
+ error()
+checkupdates = input("Do you want to check updates?(Y/N)")
+if checkupdates != 'y' and checkupdates != 'n' and checkupdates != 'Y' and checkupdates != 'N' :
+ print("ERORR[Code:1]")
+ error()
+if checkupdates == 'y' or checkupdates == 'Y' : 
+  os.system("php upd.php")
+  clear()
+  print("Start again!(python start.py)")
+  os.system("exit")
+print("Press ENTER if you want to skip.")
+debugi = input("Debug(Y)?:")
+if multitoken1 > 1:
+ multitoken = True
+if debugi == 'Y' : 
+ debug = True
+clear()
+n = 1
+enl = False
+if multitoken1 == 1 : 
+ if type == 1 and os.path.isfile("C:/token.txt") : 
+   loadtoken = input("Load last token?(y/n): ")
+   if loadtoken == 'y' : 
+     tokenfile1 = open("C:/token.txt")
+     TOKEN_1 = tokenfile1.read()
+ elif type == 2 and os.path.isfile('token'): 
+   loadtoken = input("Load last token?(y/n): ")
+   if loadtoken == 'y' : 
+     tokenfile1 = open("token")
+     TOKEN_1 = tokenfile1.read()
+ else : loadtoken = 'n'
+ if loadtoken == 'n' :
+  TOKENlink = input('Access token link:')
+  TOKEN1 = TOKENlink.rsplit('access_token=')
+  TOKEN2 = TOKEN1[1]
+  TOKEN3 = TOKEN2.rsplit('&expires_in')
+  TOKEN_1 = TOKEN3[0]
+  savetoken = input('Save token?(y/n): ')
+ else :
+  print("Access token is:" + str(TOKEN))
+elif multitoken1 != 1 and multitoken1 <= 20 :
+ en1 = True
+ if type == 1 and os.path.isfile("C:/multitoken.txt") : 
+  if multitoken1 == len(re.findall(r"[\n']+", open('C:/multitoken.txt').read())):
+   loadmultitoken = input("Load last multitoken?(y/n): ")
+   if loadmultitoken == 'y' : 
+     multitokenfile1 = open("C:/multitoken.txt")
+     for n in range(0,multitoken1):
+      tokenl1 = multitokenfile1.readline()
+      tokenl2 = tokenl1.rsplit("\n")
+      tokenlist[n] = tokenl2[0]
+ elif type == 2 and os.path.isfile('multitoken'): 
+  if multitoken1 == len(re.findall(r"[\n']+", open('C:/multitoken.txt').read())):
+   loadmultitoken = input("Load last token?(y/n): ")
+   if loadtoken == 'y' : 
+     multitokenfile1 = open("multitoken")
+     for n in range(0,multitoken1):
+      tokenl1 = multitokenfile1.readline()
+      tokenl2 = tokenl1.rsplit("\n")
+      tokenlist[n] = tokenl2[0]()
+ else : 
+  loadmultitoken = 'n'
+ if loadmultitoken == 'n':
+   for n in range(0,multitoken1) :
+    TOKENlinks = input("Token "+ str(n+1) + " is:")
+    TOKEN1s = TOKENlinks.rsplit('access_token=')
+    TOKEN2s = TOKEN1s[1]
+    TOKEN3s = TOKEN2s.rsplit('&expires_in')
+    tokenlist[n] = TOKEN3s[0]
+    n += 1
+   savemultitoken = input("Save multitoken?:")
+   if loadmultitoken == 'n':
+    if type == 1 and savemultitoken == 'y' :
+     multitokenfile = open('C:/multitoken.txt', 'w')
+     for n in range(0,multitoken1+1):
+      multitokenfile.write(str(tokenlist[n])+"\n")
+     multitokenfile.close()
+    elif type == 2 and savemultitoken == 'y' :
+     multitokenfile = open('multitoken', 'w')
+     for n in range(0,multitoken1+1):
+      multitokenfile.write(str(tokenlist[n])+"\n")
+     multitokenfile.close()
  
 time.sleep(1)
+
 if type == 1 : os.system("cls")
 elif type == 2: os.system("clear")
 post_coment = 0
@@ -64,23 +150,14 @@ fromgroupmess = int(input("Group ID(0 if you want post with your page):"))
 print("Use Default if you use page access token!")
 typem = int(input('Type:Default(1) , Jitter(2) , Custom(3):'))
 if typem != 1 and typem != 2 and typem !=3 :
- print("ERROR[Code:2]")
- if type == 1 :
-  os.system("pause")
-  os.system("exit")
- elif type == 2 :
-  os.system("exit")
+ print('ERROR[Code:1]')
+ 
 if typem == 3 : 
- print("Select number of comms in wave")
- print("Write number between 3 and 7:")
- typem = int(input())
- if typem < 3 and typem > 7 : 
-  print("ERROR[Code:3]")
-  if type == 1 :
-   os.system("pause")
-   os.system("exit")
-  elif type == 2 :
-   os.system("exit")
+  print("Select number of comms in wave")
+  print("Write number between 3 and 7:")
+  typem = int(input())
+  if typem < 3 and typem > 7 :
+   print("ERROR[Code:1]")
  
 if type == 1 : os.system("cls")
 elif type == 2: os.system("clear")
@@ -114,9 +191,9 @@ elif linktype == 2 :
   print('list 2 is:' + str(linksplit3))
   print(' ')
   print('list 3 is:' + str(linksplit4))
- 
-session = vk.Session(access_token=TOKEN)
-api = vk.API(session ,v='5.92', lang='ru')
+if multitoken == False:
+ session = vk.Session(access_token=TOKEN)
+ api = vk.API(session ,v='5.92', lang='ru')
 
 if debug == True :
  print('postID is:' + str(postID))
@@ -138,25 +215,50 @@ else :
 time.sleep(3)
 print("Started")
 time.sleep(2)
-counter = 0
+
 rangeclamp = typem + 1
-while counter != 999:
+mcounter = 0
+
+print(tokenlist)
+ 
+while counter != -1:
+   if multitoken == True :
+    for n in range(0,multitoken1):
+      session = vk.Session(access_token=tokenlist[n])
+      api = vk.API(session ,v='5.92', lang='ru')
+      if linktype == 1 :
+       (api.wall.createComment(owner_id=post_coment,post_id=postID,from_group=fromgroupmess,message=mess))
+       mcounter += 1
+      elif linktype == 2 :
+       (api.photos.createComment(owner_id=post_coment,photo_id=postID,from_group=fromgroupmess,message=mess))
+       mcounter += 1
+    if type == 1 : os.system("cls")
+    elif type == 2: os.system("clear")
+    print("       •Comment++•")
+    print("       •Multitoken•")
+    print("Already sent:" + str(mcounter))
+    print("Message:" + str(mess))
+    print("Mess in wave:" + str(typem))
+    print("Tokens:"+str(multitoken1))
+    print("Powered by Termux-Lab")
+    print("by aFro(vk.com/sorrybutiloveanother)")
+   else:
     if linktype == 1 : 
      for n in range(1,rangeclamp) :
-       (api.wall.createComment(owner_id=post_coment,post_id=postID,from_group=fromgroupmess,message=mess))
-       counter += 1
+      (api.wall.createComment(owner_id=post_coment,post_id=postID,from_group=fromgroupmess,message=mess))
+      counter += 1
     elif linktype == 2 :
      for n in range(1,rangeclamp) :     
         (api.photos.createComment(owner_id=post_coment,photo_id=postID,from_group=fromgroupmess,message=mess))
         counter += 1
-    
     if type == 1 : os.system("cls")
     elif type == 2: os.system("clear")
     print("       •Comment++•")
     print("Already sent:" + str(counter))
     print("Message:" + str(mess))
     print("Mess in wave:" + str(typem))
+    print("")
     print("Powered by Termux-Lab")
-    print("by aFro(Ivan Vakushin)")
+    print("by aFro(vk.com/sorrybutiloveanother)")
     
     time.sleep(int(coldown))
